e2b9c0c9e644c15713d107fcde94cd375a95ac70
Taddeus Kroes <taddeuskroes@gmail.com>
2018-12-10 14:17:14 +0100

    Solve day 10

---
+++ 10_stars.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import sys
+import re
+
+x, y, dx, dy = map(list, zip(*(map(int, re.findall(r'-?\d+', l)) for l in sys.stdin)))
+h = 1 << 63
+prevh = h + 1
+t = -1
+while h < prevh:
+    prevh = h
+    for i in range(len(x)):
+        x[i] += dx[i]
+        y[i] += dy[i]
+    h = max(y) - min(y)
+    t += 1
+
+for i in range(len(x)):
+    x[i] -= dx[i]
+    y[i] -= dy[i]
+
+print('message after', t, 'seconds:')
+points = set(zip(x, y))
+for py in range(min(y), max(y) + 1):
+    print(''.join('#' if (px, py) in points else '.'
+          for px in range(min(x), max(x) + 1)))
