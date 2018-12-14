7aa08902298ee5b018ebe8401c132792e2238e97
Taddeus Kroes <taddeuskroes@gmail.com>
2018-12-14 12:46:23 +0100

    Shorten day 14 solution

--- 14_recipes.py
+++ 14_recipes.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-n = 440231
-digits = []
-s = n
+s = n = 440231
+nb = bytes()
 while s:
-    digits.append(s % 10)
-    s //= 10
-nb = bytes(reversed(digits))
+    s, d = divmod(s, 10)
+    nb = bytes((d,)) + nb
 scores = bytearray((3, 7))
 a, b = 0, 1
 
@@ -13,7 +11,7 @@ while len(scores) <= 11 + n or nb not in scores[-8:]:
     sa = scores[a]
     sb = scores[b]
     s = sa + sb
-    if s > 9:
+    if s >= 10:
         scores.append(s // 10)
     scores.append(s % 10)
     a = (a + 1 + sa) % len(scores)
