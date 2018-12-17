86304d4d1d2f0e6acd951e2484cff6ba7392feb1
Taddeus Kroes <taddeuskroes@gmail.com>
2018-12-18 00:15:20 +0100

    Allow custom interpreter in runall script

--- runall.sh
+++ runall.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+interp=${1-python3}
 i=1
 for f in *.py
 do
     echo "-- problem $i -----------------------------------"
-    cmd="`which time` -f 'elapsed: %E' ./$f"
+    cmd="`which time` -f 'elapsed: %E' $interp $f"
     if [ -e input/$i ]
     then cmd+=" < input/$i"
     fi
