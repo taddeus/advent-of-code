814032062a8f56818cc528cddc93e7d6ccd5ef60
Taddeus Kroes <taddeuskroes@gmail.com>
2020-12-05 13:24:38 +0100

    Make 2020 day 5 solution linear

--- 2020/05_boardingpass.py
+++ 2020/05_boardingpass.py
@@ -2,6 +2,7 @@
 import sys
 
 tr = str.maketrans('FBLR', '0101')
-seats = sorted(int(line.translate(tr), 2) for line in sys.stdin)
-print(max(seats))
-print(next(s + 1 for i, s in enumerate(seats[:-1]) if seats[i + 1] == s + 2))
+seats = [int(line.translate(tr), 2) for line in sys.stdin]
+lo, hi = min(seats), max(seats)
+print(hi)
+print(int((lo + (hi - lo) / 2) * (len(seats) + 1)) - sum(seats))
