6dc283f250726a2a3f8eb11d730aec1446aec25a
Taddeus Kroes <taddeuskroes@gmail.com>
2022-12-06 09:51:05 +0100

    Solve day 6

---
+++ 2022/06_packets.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+from collections import Counter, deque
+
+def marker(stream, winlen):
+    window = deque(stream[:winlen - 1])
+    for i, char in enumerate(stream[winlen - 1:]):
+        if char not in window and len(Counter(window)) == winlen - 1:
+            return i + winlen
+        window.popleft()
+        window.append(char)
+
+stream = sys.stdin.readline().rstrip()
+print(marker(stream, 4))
+print(marker(stream, 14))
