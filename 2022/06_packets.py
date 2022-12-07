88f62f27cfde486de0cf2e13f7ec8508fb52e84e
Taddeus Kroes <taddeuskroes@gmail.com>
2022-12-07 10:32:20 +0100

    Minor cleanup

--- 2022/06_packets.py
+++ 2022/06_packets.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import sys
-from collections import Counter, deque
+from collections import deque
 
 def marker(stream, winlen):
     window = deque(stream[:winlen - 1])
     for i, char in enumerate(stream[winlen - 1:]):
-        if char not in window and len(Counter(window)) == winlen - 1:
+        if char not in window and len(set(window)) == winlen - 1:
             return i + winlen
         window.popleft()
         window.append(char)
