b04496af7852c73ec1d8659a1ed8499088582b80
Taddeus Kroes <taddeuskroes@gmail.com>
2022-12-12 00:41:30 +0100

    Fix typo in runall script

--- runall.sh
+++ runall.sh
@@ -3,7 +3,7 @@ python=${PYTHON:-python3}
 trap exit SIGINT
 for year in "$@"; do
     day=1
-    for solution in $year/[012]*.py do
+    for solution in $year/[012]*.py; do
         echo "-- year $year -- day $day -------------------------------"
         [ -e $year/input/$day ] && inp=$year/input/$day || inp=/dev/null
         `which time` -f 'elapsed: %E' "$python" $solution < $inp
