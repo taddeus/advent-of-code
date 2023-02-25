c6acf2750e556ddc9712034f3110e1d87a1671b3
Taddeus Kroes <taddeuskroes@gmail.com>
2023-02-25 02:26:21 +0100

    Make runall script compatible with macos with gnu-time installed

--- runall.sh
+++ runall.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 python=${PYTHON:-python3}
 trap exit SIGINT
+time=`which gtime` || time=`which time`
 for year in "$@"; do
     day=1
     for solution in $year/[012]*.py; do
         echo "-- year $year -- day $day -------------------------------"
         [ -e $year/input/$day ] && inp=$year/input/$day || inp=/dev/null
-        `which time` -f 'elapsed: %E' "$python" $solution < $inp
+        $time -f 'elapsed: %E' "$python" $solution < $inp
         echo
         day=$((day + 1))
     done
